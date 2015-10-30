f15d28db51c840a74994406af501aa0c68830b8e
Knudian <3Lra!s3r>
2015-10-30 19:23:32 +0100

    Translation update

--- js/application/providers/translate.js
+++ js/application/providers/translate.js
@@ -1,12 +1,24 @@
 /* ===== Translate provider ===== */
 app.config(['$translateProvider', function ($translateProvider) {
-    // $translateProvider.translations('fr', translations.FR);
+    $translateProvider
+        .translations('en', translations.EN)
+        .translations('fr', translations.FR)
+        .preferredLanguage('en')
+        .fallbackLanguage('en')
+        .useSanitizeValueStrategy('escape');
+    
+    /*
+        The translation system should look like that ... but not.
+        
+        Need to find a way to use it correctly.
+    
     $translateProvider
         .useStaticFilesLoader({
-            prefix: 'langs/',
+            prefix: 'locale-',
             suffix: '.json'
         })
         .preferredLanguage('en')
         .fallbackLanguage('en')
         .useSanitizeValueStrategy('escape');
+    */
 }]);
