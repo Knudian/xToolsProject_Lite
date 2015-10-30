227cadc97f4152c0e751054644a3aaf87478e07f
Knudian <kevin.meche.pro@gmail.com>
2015-10-30 10:14:43 +0100

    Update routes.js

--- js/application/providers/routes.js
+++ js/application/providers/routes.js
@@ -4,8 +4,8 @@ app.config(['$routeProvider', function ($routeProvider){
         .when('/', {redirectTo: '/home'})
         .when('/home', {templateUrl: 'partials/home.html'})
         .when('/error', {templateUrl: 'partials/error.html'})
-        .when('/hammerbuilder', {templateUrl: 'partials/tools/hammerbuilder.html', controller:'HB_Controller'})
-        .when('/tsa', {templateUrl: 'partials/tools/tsa.html', controller:'TSA_Controller'})
-        .when('/comparator', {templateUrl: 'partials/helpers/troop_research.html', controller:'TComparator_Controller'})
+        .when('/hammerbuilder', {templateUrl: 'partials/hammerbuilder.html', controller:'HB_Controller'})
+        .when('/tsa', {templateUrl: 'partials/tsa.html', controller:'TSA_Controller'})
+        .when('/comparator', {templateUrl: 'partials/troop_research.html', controller:'TComparator_Controller'})
         .otherwise({redirectTo: '/error'});
 }]);
